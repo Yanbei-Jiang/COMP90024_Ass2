9cfdaa3a1c48e919c8c1d75890b33ff0238aeca2
Wei Ge <gewg@student.unimelb.edu.au>
2022-04-27 00:40:41 +1000

    Finish template to READ/WRITE data to couchDB

--- crawler/db_load_data.py
+++ crawler/db_load_data.py
@@ -1,5 +1,27 @@
 import configparser
 import os
+import couchdb
+import json
+
+
+def connect_couchdb(username, password, host, port):
+    '''
+    Connect to couchdb
+    '''
+    couch = couchdb.Server('http://' + username + ':' + password + '@' + host + ':' + port)
+    return couch
+
+
+def get_spec_db(db_name, couch):
+    '''
+    Get the specific database
+    '''
+    # get existed db
+    try:
+        return couch[db_name]
+    # otherwise, create the db
+    except:
+        return couch.create(db_name)
 
 
 def get_db_into():
@@ -27,6 +49,31 @@ def get_db_into():
     return dict
 
 
+def store_dynamic_to_db(db_name, data):
+    '''
+    Accept dynamic data and store into specific database
+    '''
+    db_name.save(data)
+
+
+def get_data_from_db(db_name):
+    '''
+    Get data from couchdb
+    '''
+    # get all rows from 
+    for id in db_name:
+        print(db_name[id])
+
+
+def empty_spec_db(couch, db_name):
+    '''
+    Delete all data in the specific database
+    @param db: the couchdb
+    @param db_name: the specific database be deleted
+    '''
+    couch.delete(db_name)
+
+
 # get the user info and db info
 db_info = get_db_into()
 username = db_info['username']
@@ -34,3 +81,17 @@ password = db_info['password']
 host = db_info['host']
 port = db_info['port']
 db_name = db_info['db_name']
+
+# connect the database
+couch = connect_couchdb(username, password, host, port)
+# get the twitter database
+dynamic_twitter_db = get_spec_db('dynamic_twitter', couch)
+old_twitter_db = get_spec_db('old_twitter', couch)
+
+# store data to db
+with open("test.json", 'r') as f:
+    curr_row = json.load(f)
+    store_dynamic_to_db(dynamic_twitter_db, curr_row)
+
+# read from data
+get_data_from_db(dynamic_twitter_db)
