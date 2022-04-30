e00f42c111247aa162908ff3e20434e0d97c9062
gewg <1821691929@qq.com>
2022-04-30 16:23:47 +0800

    FIX BUG

--- crawler/crawl_data.py
+++ crawler/crawl_data.py
@@ -11,14 +11,14 @@ def set_configuration():
     Set the configuration for crawlers
     '''
     # get the key words for each crawler to search
-    keywords_file = './search_keywords.json'
+    keywords_file = 'search_keywords.json'
     # read the json with key words
     keywords = []
     with io.open(keywords_file,'r') as f:
         keywords = json.load(f)
 
     # get the configuration of crawler
-    config_file = './crawler_config.ini'
+    config_file = 'crawler_config.ini'
     # read the configuration
     try:
         # read the configuration file
@@ -75,7 +75,7 @@ def initialize_db():
 initialize_db()
 
 # start the crawlers
-start_crawlers()
+# start_crawlers()
 
 # empty the db
 #db_load_data.empty_spec_db('original_tweets')
--- crawler/db_load_data.py
+++ crawler/db_load_data.py
@@ -11,9 +11,10 @@ username = ""
 password = ""
 host = ""
 port = ""
-odl_db_name = ""
+old_db_name = ""
 new_db_name = ""
 couch = ""
+
 
 def connect_couchdb(username, password, host, port):
     '''
@@ -61,14 +62,6 @@ def get_db_into():
     return dict
 
 
-<<<<<<< HEAD
-def store_dynamic_to_db(db_name, data):
-    '''
-    Accept dynamic data and store into specific database
-    '''
-    db_name.save(data)
-
-=======
 def initialize_couchdb():
     '''
     Initialize the couchdb
@@ -81,18 +74,21 @@ def initialize_couchdb():
     global odl_db_name
     global new_db_name
     global couch
-
+    
     # set up info
     db_info = get_db_into()
     username = db_info['username']
     password = db_info['password']
     host = db_info['host']
     port = db_info['port']
-    odl_db_name = db_info['odl_db_name']
+    old_db_name = db_info['old_db_name']
     new_db_name = db_info['new_db_name']
 
     # connect the database
     couch = connect_couchdb(username, password, host, port)
+    # generate the two database
+    get_spec_db(old_db_name, couch)
+    get_spec_db(new_db_name, couch)
     print("Initialize the database Successfully")
 
 
@@ -101,9 +97,9 @@ def store_to_backup_db(data):
     Accept data and store into backup database
     @param data be stored
     '''
-    global odl_db_name
+    global old_db_name
     global couch
-    get_spec_db(odl_db_name, couch).save(data)
+    get_spec_db(old_db_name, couch).save(data)
 
 
 def store_to_processed_db(data):
@@ -115,57 +111,22 @@ def store_to_processed_db(data):
     global couch
     get_spec_db(new_db_name, couch).save(data)
     
->>>>>>> 632a4a4f924fe7e7c41e7e25da2962d32590894e
 
 def get_data_from_db(db_name):
     '''
     Get data from couchdb
-<<<<<<< HEAD
-=======
     @param db_name
->>>>>>> 632a4a4f924fe7e7c41e7e25da2962d32590894e
     '''
     # get all rows from 
     for id in db_name:
         print(db_name[id])
 
 
-<<<<<<< HEAD
-def empty_spec_db(couch, db_name):
-=======
 def empty_spec_db(db_name):
->>>>>>> 632a4a4f924fe7e7c41e7e25da2962d32590894e
     '''
     Delete all data in the specific database
     @param db: the couchdb
     @param db_name: the specific database be deleted
     '''
-<<<<<<< HEAD
-    couch.delete(db_name)
-
-
-# get the user info and db info
-db_info = get_db_into()
-username = db_info['username']
-password = db_info['password']
-host = db_info['host']
-port = db_info['port']
-db_name = db_info['db_name']
-
-# connect the database
-couch = connect_couchdb(username, password, host, port)
-# get the twitter database
-dynamic_twitter_db = get_spec_db('dynamic_twitter', couch)
-old_twitter_db = get_spec_db('old_twitter', couch)
-
-# store data to db
-with open("test.json", 'r') as f:
-    curr_row = json.load(f)
-    store_dynamic_to_db(dynamic_twitter_db, curr_row)
-
-# read from data
-get_data_from_db(dynamic_twitter_db)
-=======
     global couch
     couch.delete(db_name)
->>>>>>> 632a4a4f924fe7e7c41e7e25da2962d32590894e
