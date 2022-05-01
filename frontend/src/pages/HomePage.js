643bd5d481a2b6216aa2938d761122fbca149cd6
Lisple <yiwzhang1@student.unimelb.edu.au>
2022-05-01 18:29:55 +0800

    add frontend, the current map data is only for testing map functions

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import KMaps from "./KeplerMap"
+
+export default function HomePage() {
+    return(
+        <KMaps/>
+    )
+}
---
+++ frontend/src/pages/KeplerMap.js
@@ -0,0 +1,66 @@
+import React, {useEffect} from 'react';
+import keplerGlReducer from 'kepler.gl/reducers';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {taskMiddleware} from 'react-palm/tasks';
+import { Provider, useDispatch } from 'react-redux';
+import KeplerGl from "kepler.gl";
+import { addDataToMap } from "kepler.gl/actions";
+import useSwr from "swr";
+
+const reducers = combineReducers({
+    keplerGl: keplerGlReducer
+});
+  
+const store = createStore(reducers, {}, applyMiddleware(taskMiddleware));
+
+export default function keplerMap (){
+    return(
+        <Provider store={store}>
+            <Map />
+        </Provider>
+    )
+}
+
+
+
+function Map() {
+    const dispatch = useDispatch();
+    const {data} = useSwr("covid", async () =>{
+        const response = await fetch(
+            "https://gist.githubusercontent.com/leighhalliday/a994915d8050e90d413515e97babd3b3/raw/a3eaaadcc784168e3845a98931780bd60afb362f/covid19.json"
+        );
+        const data = await response.json();
+        console.log(data)
+        return data;
+    });
+
+    useEffect(() => {
+        if (data) {
+            dispatch(
+                addDataToMap({
+                    datasets: {
+                        info: {
+                            label: "COVID-19",
+                            id: "covid19"
+                        },
+                        data:data
+                    },
+                    option: {
+                        centerMap: true,
+                        readOnly: false
+                    },
+                    config: {}
+                })
+            );
+        }
+    }, [dispatch, data]);
+
+    return(
+        <KeplerGl
+            id="covid"
+            mapboxApiAccessToken= {process.env.REACT_APP_MAPBOX_API}
+            width={window.innerWidth}
+            height={window.innerHeight}
+        />
+    )
+}
