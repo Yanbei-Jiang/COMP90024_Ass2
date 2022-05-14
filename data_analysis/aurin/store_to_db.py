b9edd8c628c551279ed0eb6f2e118584fe883cae
hanwang2 <269169165@qq.com>
2022-05-14 21:33:46 +0800

    updata aurin

--- data_analysis/aurin/store_to_db.py
+++ data_analysis/aurin/store_to_db.py
@@ -1,4 +1,22 @@
 import sys
-sys.path.append('../../crawler/')
-import db_load_data.py as db
+import json
+sys.path.append("..\..\crawler")
+print(sys.path)
+import db_load_data
+
 doc_education_level = "education_level.json"
+doc_housing_price = "housing_price.json"
+
+doc_education_metadata = "education_metadata.json"
+doc_education_origin_data = "education_origin_data.json"
+doc_house_origin_data = "house_origin_data.json"
+doc_house_price_metadata = "house_price_metadata.json"
+
+db_load_data.initialize_couchdb()
+
+json.load(doc_education_level)
+json.load(doc_housing_price)
+json.load(doc_education_metadata)
+json.load(doc_education_origin_data)
+json.load(doc_house_origin_data)
+json.load(doc_house_price_metadata)
