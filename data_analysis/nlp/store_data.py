a863e935b395f1eca75df4e6628bca1f2795d281
Yanbei-Jiang <yanbeij@student.unimelb.edu.au>
2022-05-14 23:14:04 +1000

    fix bugs

--- data_analysis/nlp/store_data.py
+++ data_analysis/nlp/store_data.py
@@ -1,5 +1,19 @@
-def store_to_db():
+import sys
+sys.path.append("..\..\crawler")
+from db_load_data import *
+from main import *
+
+initialize_couchdb()
+old_tweets = get_spec_db("old_tweets")
+original_tweets = get_spec_db("original_tweets")
+
+for tweet in old_tweets:
+    processed = old_tweet_analysis(tweet)
     
+    store_to_processed_db(processed)
 
+for tweet in original_tweets:
+    processed = new_tweet_analysis(tweet)
 
-store_to_db()
+    store_to_processed_db(processed)
+
