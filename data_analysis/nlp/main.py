cc35981e116fc60db72d17165081864bd8a2423e
Yanbei-Jiang <yanbeij@student.unimelb.edu.au>
2022-05-15 00:09:54 +1000

    Update main.py

--- data_analysis/nlp/main.py
+++ data_analysis/nlp/main.py
@@ -4,27 +4,58 @@ from sentiment_polarity import *
 
 def old_tweet_analysis(tweet_obj):
     text = tweet_obj["doc"]["text"]
-    return process_tweet(text)
-
-
-def new_tweet_analysis(tweet_obj):
-    for key in tweet_obj.keys():
-        # only look at the third key (i.e. "Hoppers_Crossing_1522304716188700672_stream")
-        if tweet_obj[key] != "_id" and tweet_obj[key] != "_rev":
-            if "extended_tweet" in tweet_obj[key].keys():
-                text = tweet_obj[key]["extended_tweet"]["full_text"]
-            else:
-                text = tweet_obj[key]["text"]
-    return process_tweet(text)
-
-
-def process_tweet(text):
+    coordinates = tweet_obj["doc"]["place"]["bounding_box"]["coordinates"][0]
+    area = filter_area(coordinates)
+    tweet_obj["melbourne_area"] = area
     processed_text = preprocess(text)
     sentiment = sentiment_analysis(processed_text)
     tweet_obj["sentiment"] = sentiment
     polarity = polarity_analysis(processed_text)
     tweet_obj["polarity"] = polarity
     return tweet_obj
+
+
+def new_tweet_analysis(tweet_obj):
+    for key in tweet_obj.keys():
+        # only look at the third key (i.e. "Hoppers_Crossing_1522304716188700672_stream")
+        if tweet_obj[key] != "_id" and tweet_obj[key] != "_rev":
+            coordinates = tweet_obj[key]["place"]["bounding_box"]["coordinates"][0]
+            area = filter_area(coordinates)
+            tweet_obj["melbourne_area"] = area
+            if "extended_tweet" in tweet_obj[key].keys():
+                text = tweet_obj[key]["extended_tweet"]["full_text"]
+                break
+            else:
+                text = tweet_obj[key]["text"]
+                break
+    processed_text = preprocess(text)
+    sentiment = sentiment_analysis(processed_text)
+    tweet_obj["sentiment"] = sentiment
+    polarity = polarity_analysis(processed_text)
+    tweet_obj["polarity"] = polarity
+    return tweet_obj
+
+
+def filter_area(coordinates):
+    melbourne_city = [144.913581,-37.839086,144.994266,-37.781922]
+    melbourne_kew = [144.999392,-37.855040,145.141140,-37.786685]
+    Hoppers_Crossing = [144.633666,-37.974500,144.913581,-37.694900]
+
+    left_bound = coordinates[0][0]
+    right_bound = coordinates[2][0]
+    top_bound = coordinates[1][1]
+    down_bound = coordinates[0][1]
+    center = [(left_bound+right_bound)/2, (top_bound+down_bound)/2]
+    coordinates.append(center)
+    for coordinate in coordinates:
+        if Hoppers_Crossing[0] <= coordinate[0] <= Hoppers_Crossing[2] and Hoppers_Crossing[1] <= coordinate[1] <= Hoppers_Crossing[3]:
+            return "Hoppers_Crossing"
+        elif melbourne_kew[0] <= coordinate[0] <= melbourne_kew[2] and melbourne_kew[1] <= coordinate[1] <= melbourne_kew[3]:
+            return "melbourne_kew" 
+        elif melbourne_city[0] <= coordinate[0] <= melbourne_city[2] and melbourne_city[1] <= coordinate[1] <= melbourne_city[3]:
+            return "melbourne_city"
+
+    return "other"
 
 
 def preprocess(text):
