d46022666dd88ae69df5045551d55a82d25137af
Yanbei-Jiang <yanbeij@student.unimelb.edu.au>
2022-05-15 17:09:07 +1000

    Update store_to_db.py

--- data_analysis/aurin/store_to_db.py
+++ data_analysis/aurin/store_to_db.py
@@ -5,7 +5,7 @@ from db_load_data import *
 import json 
 
 def main():
-    doc_education_level = open("education_level.json",'r')
+    doc_education_level = open("aurin/education_level.json",'r')
     doc_housing_price = open("aurin/housing_price.json",'r')
     doc_education_metadata = open("aurin/education_metadata.json",'r')
     doc_education_origin_data = open("aurin/education_origin_data.json",'r')
