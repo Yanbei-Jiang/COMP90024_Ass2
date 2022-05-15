ab70fec9e5235bcb8e0a314f0f72853c4b8ef477
Yanbei-Jiang <yanbeij@student.unimelb.edu.au>
2022-05-16 01:29:11 +1000

    Update main.py

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -1,9 +1,9 @@
 # import aurin.store_to_db 
 # import nlp.store_data 
-
+import nltk
 
 from aurin import store_to_db
 from nlp import store_data
 #store_to_db.aurin_main()
-
+nltk.download('vader_lexicon')
 store_data.nlp_main()
