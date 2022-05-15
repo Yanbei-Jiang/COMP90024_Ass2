38fc8219886e648ca41d81939e8a7a483d556c1d
Yanbei-Jiang <yanbeij@student.unimelb.edu.au>
2022-05-16 02:43:29 +1000

    Update store_data.py

--- data_analysis/nlp/store_data.py
+++ data_analysis/nlp/store_data.py
@@ -10,18 +10,18 @@ def nlp_main():
     initialize_couchdb()
     old_tweets = get_spec_db("old_tweets")
     original_tweets = get_spec_db("original_tweets")
-    count = 0
+    #count = 0
     for tweet_old, tweet_new in zip_longest(old_tweets,original_tweets):
         if tweet_old:            
-            count += 1            
-            if count <= 3129:
-                processed_old = old_tweet_analysis(old_tweets[tweet_old])
-                store_to_processed_db(processed_old)
+            # count += 1            
+            # if count >= 3129:
+            processed_old = old_tweet_analysis(old_tweets[tweet_old])
+            store_to_processed_db(processed_old)
 
         if tweet_new:             
-            count += 1
-            if count <= 3129:
-                processed_new = new_tweet_analysis(original_tweets[tweet_new])
-                store_to_processed_db(processed_new)
+            # count += 1
+            # if count >= 3129:
+            processed_new = new_tweet_analysis(original_tweets[tweet_new])
+            store_to_processed_db(processed_new)
 
     
